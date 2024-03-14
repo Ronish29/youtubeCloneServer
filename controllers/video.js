49098eac8d1a417752b6401687d20a9948ca2710
Ronish29 <ronishparmar2003@gmail.com>
2024-03-14 21:36:17 +0530

    quality changed

--- controllers/video.js
+++ controllers/video.js
@@ -16,9 +16,9 @@ exports.uploadVideo = async (req, res, next,io) => {
     const currentDate = new Date().toISOString().replace(/:/g, "-") + "-" ;
     const qualities = [
       { name: '1080p', width: 1920, height: 1080 },
-      { name: '720p', width: 1280, height: 720 },
-      { name: '480p', width: 854, height: 480 },
-      { name: '360p', width: 560, height: 320 }
+      // { name: '720p', width: 1280, height: 720 },
+      // { name: '480p', width: 854, height: 480 },
+      // { name: '360p', width: 560, height: 320 }
     ];
 
 
